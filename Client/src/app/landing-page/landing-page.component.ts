41786c4a85f725088c48106f81fc30c0e01886b4
rradarr <radorze093@student.polsl.pl>
2022-04-10 00:05:51 +0200

    Fix landing page layout

--- Client/src/app/landing-page/landing-page.component.ts
+++ Client/src/app/landing-page/landing-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
-  host: {'class': 'flex-auto flex justify-center items-start'}, // ! Styling host container to fill all avialable space
+  host: {'class': 'flex-auto'}, // ! Styling host container to fill all avialable space
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css'],
 })

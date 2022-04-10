fccfda39ad3b974cbe5d17a4b5c189fd8e5051af
rradarr <radorze093@student.polsl.pl>
2022-04-10 13:52:24 +0200

    Fix document scaling
    All elements are now in flex containers which allows main
    content of the page to fill the screen and keep footer always at the bottom.

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -4,7 +4,8 @@ import { Meta, MetaDefinition } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  host: { 'class' : 'flex flex-col grow'}
 })
 
 export class AppComponent implements OnInit {
--- Client/src/app/landing-page/landing-page.component.ts
+++ Client/src/app/landing-page/landing-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
-  host: {'class': 'flex-auto'}, // ! Styling host container to fill all avialable space
+  host: {'class': 'grow flex items-center'}, // ! Styling host container to fill all avialable space
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css'],
 })
